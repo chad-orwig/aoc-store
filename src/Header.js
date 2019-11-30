37bb98040256aaa20ee03a27166aaa96d4e914ef
Chad Orwig <chad.orwig@slalom.com>
2019-11-30 13:02:23 -0500

    row and col for header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import LoginPanel from './LoginPanel'
-import ControlPanel from './ControlPanel'
+import LoginPanel from './LoginPanel';
+import ControlPanel from './ControlPanel';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import { itemType } from './types';
 import { arrayOf, func, object } from 'prop-types';
@@ -10,9 +12,13 @@ function Header({ user, items, addAlert }) {
     const loginPanel = !user ? (<LoginPanel />) : (<ControlPanel user={user} items={items} addAlert={addAlert}/>);
     return (
         <div className="fixed-top bg-info text-white p-2">
-            <h2 className="d-inline-block d-sm-none">AOC</h2>
-            <h2 className="d-none d-sm-inline-block">AOC Star Store</h2>
-            { loginPanel }
+            <Row noGutters={true}>
+                <Col xs={0} sm={4}>
+                    <h2 className="d-none d-sm-inline-block d-md-none">AOC</h2>
+                    <h2 className="d-none d-md-inline-block">AOC Star Store</h2>
+                </Col>
+                <Col xs={12} sm={8}>{ loginPanel }</Col>
+            </Row>
         </div>
     )
 }
