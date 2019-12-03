b6d5c4ed7136fbffc1b4f8dbdc0f79998d457ad8
Chad Orwig <chad.orwig@slalom.com>
2019-12-03 08:39:32 -0500

    overlay needs position fixed

--- src/DisabledOverlay.js
+++ src/DisabledOverlay.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function DisabledOverlay() {
     return (
-        <div style = {{top: 0, bottom: 0, left: 0, right: 0, zIndex : 5000, position : 'absolute', background : '#77777777' }}>
+        <div style = {{top: 0, bottom: 0, left: 0, right: 0, zIndex : 5000, position : 'fixed', background : '#77777777' }}>
         </div>
     );
 }
